1bb7160d9fa98e5b2cb8b0a33e7f958b36a0bcbe
sho94os <65626166+sho94os@users.noreply.github.com>
2020-06-07 01:56:11 +0200

    Update banner.sh

--- Modulos/banner.sh
+++ Modulos/banner.sh
@@ -13,23 +13,23 @@ chk=$(cat /etc/ssh/sshd_config | grep Banner)
 }
 echo -e "\E[44;1;37m             BANNER             \E[0m"
 echo ""
-echo -e "\033[1;31m[\033[1;36m1\033[1;31m]\033[1;37m • \033[1;33mADICIONAR BANNER"
-echo -e "\033[1;31m[\033[1;36m2\033[1;31m]\033[1;37m • \033[1;33mREMOVER BANNER"
-echo -e "\033[1;31m[\033[1;36m3\033[1;31m]\033[1;37m • \033[1;33mVOLTAR"
+echo -e "\033[1;31m[\033[1;36m1\033[1;31m]\033[1;37m • \033[1;33m Add BANNER"
+echo -e "\033[1;31m[\033[1;36m2\033[1;31m]\033[1;37m • \033[1;33m Remove BANNER"
+echo -e "\033[1;31m[\033[1;36m3\033[1;31m]\033[1;37m • \033[1;33m Return"
 echo ""
-echo -ne "\033[1;32mOQUE DESEJA FAZER\033[1;31m ?\033[1;37m : "; read resp
+echo -ne "\033[1;32m WHAT DO YOU WANT TO DO\033[1;31m ?\033[1;37m : "; read resp
 if [[ "$resp" = "1" ]]; then
 echo ""
-echo -ne "\033[1;32mQUAL MENSAGEM DESEJA EXIBIR\033[1;31m ?\033[1;37m : "; read msg1
+echo -ne "\033[1;32m WHICH MESSAGE YOU WANT TO DISPLAY\033[1;31m ?\033[1;37m : "; read msg1
 if [[ -z "$msg1" ]]; then
-	echo -e "\n\033[1;31mCampo vazio ou invalido !\033[0m"
+	echo -e "\n\033[1;31mEmpty or invalid field!\033[0m"
 	sleep 2
 	banner
 fi
-echo -e "\n\033[1;31m[\033[1;36m01\033[1;31m]\033[1;33m FONTE PEQUENA"
-echo -e "\033[1;31m[\033[1;36m02\033[1;31m]\033[1;33m FONTE MEDIA"
-echo -e "\033[1;31m[\033[1;36m03\033[1;31m]\033[1;33m FONTE GRANDE"
-echo -e "\033[1;31m[\033[1;36m04\033[1;31m]\033[1;33m FONTE GIGANTE"
+echo -e "\n\033[1;31m[\033[1;36m01\033[1;31m]\033[1;33m SMALL FONT"
+echo -e "\033[1;31m[\033[1;36m02\033[1;31m]\033[1;33m   Medium Font"
+echo -e "\033[1;31m[\033[1;36m03\033[1;31m]\033[1;33m   BIG Font"
+echo -e "\033[1;31m[\033[1;36m04\033[1;31m]\033[1;33m   GIANT SOURCE"
 echo ""
 echo -ne "\033[1;32mQUAL O TAMANHO DA FONTE\033[1;31m ?\033[1;37m : "; read opc
 if [[ "$opc" = "1" ]] || [[ "$opc" = "01" ]]; then
@@ -42,16 +42,16 @@ elif [[ "$opc" = "4" ]] || [[ "$opc" = "04" ]]; then
 _size='1'
 fi
 
-echo -e "\n\033[1;31m[\033[1;36m01\033[1;31m]\033[1;33m AZUL"
-echo -e "\033[1;31m[\033[1;36m02\033[1;31m]\033[1;33m VERDE"
-echo -e "\033[1;31m[\033[1;36m03\033[1;31m]\033[1;33m VERMELHO"
-echo -e "\033[1;31m[\033[1;36m04\033[1;31m]\033[1;33m AMARELO"
-echo -e "\033[1;31m[\033[1;36m05\033[1;31m]\033[1;33m ROSA"
+echo -e "\n\033[1;31m[\033[1;36m01\033[1;31m]\033[1;33m BLUE"
+echo -e "\033[1;31m[\033[1;36m02\033[1;31m]\033[1;33m GREEN"
+echo -e "\033[1;31m[\033[1;36m03\033[1;31m]\033[1;33m RED"
+echo -e "\033[1;31m[\033[1;36m04\033[1;31m]\033[1;33m YELLOW"
+echo -e "\033[1;31m[\033[1;36m05\033[1;31m]\033[1;33m ROSE"
 echo -e "\033[1;31m[\033[1;36m06\033[1;31m]\033[1;33m CYANO"
-echo -e "\033[1;31m[\033[1;36m07\033[1;31m]\033[1;33m LARANJA"
+echo -e "\033[1;31m[\033[1;36m07\033[1;31m]\033[1;33m ORANGE"
 echo -e "\033[1;31m[\033[1;36m08\033[1;31m]\033[1;33m ROXO"
 echo -e "\033[1;31m[\033[1;36m09\033[1;31m]\033[1;33m PRETO"
-echo -e "\033[1;31m[\033[1;36m10\033[1;31m]\033[1;33m SEM COR"
+echo -e "\033[1;31m[\033[1;36m10\033[1;31m]\033[1;33m WITHOUT COLOR"
 echo ""
 echo -ne "\033[1;32mQUAL A COR\033[1;31m ?\033[1;37m : "; read ban_cor
 if [[ "$ban_cor" = "1" ]] || [[ "$ban_cor" = "01" ]]; then
@@ -79,24 +79,24 @@ echo -e "\n\033[1;32mBANNER DEFINIDO !\033[0m"
 sleep 2
 menu
 else
-echo -e "\n\033[1;31mOpcao invalida !\033[0m"
+echo -e "\n\033[1;31mOption invalid!\033[0m"
 	sleep 2
 	banner
 fi
 echo "</font></h$_size>" >> $local
 service ssh restart > /dev/null 2>&1 && service dropbear restart > /dev/null 2>&1
-echo -e "\n\033[1;32mBANNER DEFINIDO !\033[0m"
+echo -e "\n\033[1;32mBANNER DEFINED!\033[0m"
 unset ban_cor
 elif [[ "$resp" = "2" ]]; then
 	echo " " > $local
-	echo -e "\n\033[1;32mBANNER EXCLUIDO !\033[0m"
+	echo -e "\n\033[1;32mEXCLUDED BANNER!\033[0m"
 	service ssh restart > /dev/null 2>&1 && service dropbear restart > /dev/null 2>&1
 	sleep 2
 	menu
 elif [[ "$resp" = "3" ]]; then
 	menu
 else
-	echo -e "\n\033[1;31mOpcao invalida !\033[0m"
+	echo -e "\n\033[1;31mOption invalid!\033[0m"
 	sleep 2
 	banner
 fi
